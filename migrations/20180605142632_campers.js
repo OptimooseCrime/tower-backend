1a79e9228286d3cc4d2cccf4b5266cabe0b55caf
Adam Basila <adamcabasila@gmail.com>
2018-06-06 14:05:13 -0600

    add data to seeds

--- migrations/20180605142632_campers.js
+++ migrations/20180605142632_campers.js
@@ -2,7 +2,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('campers', table => {
       table.increments('id').primary()
-      table.text('image')
       table.text('username')
       table.text('bio')
   })
--- seeds/campers.js
+++ seeds/campers.js
@@ -1,29 +1,26 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex.raw("DELETE FROM 'camper'; ALTER SEQUENCE camper_id_seq RESTART WITH 3;")
+  return knex.raw("DELETE FROM 'camper'; ALTER SEQUENCE camper_id_seq RESTART WITH 4;")
     .then(function () {
       // Inserts seed entries
       let campers = [
         {
           id: 1, 
-          username:'',
-          bio:'',
+          username: 'XxSuper420GamerXx',
+          bio: "I'm only here because my parents make me go camping with them.I wanna go home and pwn noobs ",
         },
         {
           id: 2, 
-          username: '',
-          bio: ''
+          username: 'a.regular.susan',
+          bio: "I'd rather be cooking on an open fire in the middle of the woods"
         },
         {
           id: 3,
-          username: '',
-          bio: ''
+          username: "Dirtbike-Dingus",
+          bio: '"Tent camping in the sand dunes, and waking up early to ride my dirt bike. #braplife"'
         }
       ]
       return knex('camper').insert(campers)
     });
 };
-
-
-
