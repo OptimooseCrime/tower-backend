f60c2721576986cf13eae7433dc42a83d7ab66d6
Adam Basila <adamcabasila@gmail.com>
2018-06-11 13:59:50 -0600

    add cors to app

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const app = express()
 const campers = require('./routes/campers')
+const cors = require("cors")
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
-
+app.use(cors)
 app.use('/campers', campers)
 
 //catch 404 and fwd to error handler
